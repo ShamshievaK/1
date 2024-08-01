aa4a217698ba0900c03f3f48eaa682596d5384b7
shamshievak <shamshievak01@gmail.com>
2024-08-01 17:59:14 +0300

    HomeWork1

---
+++ Lessons/1.py
@@ -0,0 +1,29 @@
+"""VENV, CMD, GIT"""
+import art
+# Модули в языке их 3 вида: встроенные, собственные, внешние(скачиваются в venv)
+
+# p = 1, 'str', True, 1.2, [], {}, (), None
+# print(type(p))
+
+# Через import можно перекинуть с одного файла в другой, переносит переменную p
+# import lesson1
+# print(lesson1.p)
+
+# PIP - пакетный менеджер пишешь вначале всегжа
+# install - вставт
+
+# import colorama
+# print(colorama.Back.BLUE, colorama.Fore.BLACK)
+# print('Hello World')
+import art
+# from art import tprint
+#
+# tprint('Hello World!')
+# или
+import art
+art.tprint('Hello World!')
+
+
+
+
+Новый проект, новый репозиторий
